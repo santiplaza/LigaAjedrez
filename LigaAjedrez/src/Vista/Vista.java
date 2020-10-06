b506f85f1ac27943aa2cf4194c30e99632f0389e
Enrique_Juan <enrike1221.ej@gmail.com>
2020-10-06 12:13:24 +0200

    XD

--- LigaAjedrez/src/Vista/Vista.java
+++ LigaAjedrez/src/Vista/Vista.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ligaajedrez;
-import Vista.Vista;
+package Vista;
+
 /**
  *
  * @author enrique
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Vista vista = new Vista();
-    }
+public class Vista {
     
 }
